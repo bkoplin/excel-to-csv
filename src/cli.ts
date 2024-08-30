3e0df6fb7114e673b6e7100317065f5b457d9db8
Ben Koplin <benjaminkoplin@outlook.com>
2024-08-30 06:26:55 -0500

    feat: Improve type safety in CLI prompts
    Enhanced the type safety of the command-line interface by importing and using 'SetRequired' from 'type-fest'. This ensures that certain properties ('filePath', 'range', 'sheetName') are always present in the final answers. The prompt functions have been updated to use this new type, which will help prevent potential runtime errors due to missing or incorrectly typed properties.

--- src/cli.ts
+++ src/cli.ts
@@ -7,6 +7,7 @@ import inquirer from 'inquirer'
 
 import * as XLSX from 'xlsx'
 
+import type { SetRequired } from 'type-fest'
 import { parseWorksheet } from './index'
 
 XLSX.set_fs(fs)
@@ -29,22 +30,23 @@ const program = new Commander.Command()
   .option('-s, --sheet-name <STRING>', 'name of source worksheet')
   .option('-r, --range <STRING>', 'range of worksheet to parse')
   .action(async (args) => {
+    type FinalAnswers = SetRequired<typeof args, 'filePath' | 'range' | 'sheetName'>
     if (!args.filePath) {
-      const answer = await inquirer.prompt({ type: 'input', name: 'filePath', message: 'Enter the path to the Excel file', when: !args.filePath, required: true, validate: (filePath: string) => {
+      const answer = await inquirer.prompt<[FinalAnswers]>([{ type: 'input', name: 'filePath', message: 'Enter the path to the Excel file', when: !args.filePath, required: true, validate: (filePath: string) => {
         if (existsSync(filePath))
           return true
         else return `${colors.yellow(`"${filePath}"`)} does not exist`
-      } })
+      } }])
       args.filePath = answer.filePath
     }
     if (!args.sheetName) {
       const { SheetNames } = XLSX.readFile(args.filePath, { bookSheets: true })
-      const answer = await inquirer.prompt({ type: 'list', name: 'sheetName', message: 'Select the worksheet to parse', choices: SheetNames })
+      const answer = await inquirer.prompt<[FinalAnswers]>([{ type: 'list', name: 'sheetName', message: 'Select the worksheet to parse', choices: SheetNames }])
       args.sheetName = answer.sheetName
     }
     if (!args.range) {
       const wb = XLSX.readFile(args.filePath, { sheet: args.sheetName })
-      const answer = await inquirer.prompt({ type: 'input', name: 'range', message: 'Enter the range of the worksheet to parse', default: wb.Sheets[args.sheetName]['!ref'] })
+      const answer = await inquirer.prompt<[FinalAnswers]>([{ type: 'input', name: 'range', message: 'Enter the range of the worksheet to parse', default: wb.Sheets[args.sheetName]['!ref'] }])
       args.range = answer.range
     }
     parseWorksheet(args, spinner)
