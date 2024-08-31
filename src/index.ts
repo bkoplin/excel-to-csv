a2e61d4d277738b8d99e05b80ad734828f37e9f6
Ben Koplin <benjaminkoplin@outlook.com>
2024-08-31 08:08:41 -0500

    feat: Improve XLSX parsing logic and CSV generation
    Refactor the code in `parseWorksheet` function to enhance the XLSX parsing logic and improve the generation of CSV files. This includes updating the import statement for the `range` function from `lodash-es`, importing the `dayjs` library for date formatting, and importing the `isUndefined` function from the `@antfu/utils` package. The `processWorksheet` function is modified to use the new parsing configuration object, and the logic for extracting data from the XLSX file and generating CSV is updated. The CSV file is now written with a timestamp in the filename.
    
    This commit improves the efficiency and accuracy of parsing XLSX files and generating CSV output.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import * as fs from 'node:fs'
-import { parse } from 'node:path'
+import { join, parse } from 'node:path'
 import type { ParsedPath } from 'node:path/posix'
-import * as Commander from '@commander-js/extra-typings'
-import colors from 'picocolors'
+import { PassThrough } from 'node:stream'
 import * as XLSX from 'xlsx'
 import Papa from 'papaparse'
-import { range } from 'lodash-es'
+import { ceil, padStart, range } from 'lodash-es'
 import type { JsonValue } from 'type-fest'
 import type { Spinner } from 'yocto-spinner'
 import dayjs from 'dayjs'
-import { isUndefined } from '@antfu/utils'
+import colors from 'picocolors'
+import { confirm, number } from '@inquirer/prompts'
+import { emptyDirSync, ensureDirSync } from 'fs-extra'
 
 XLSX.set_fs(fs)
 export interface Arguments {
@@ -24,10 +25,26 @@ export async function parseWorksheet(args: {
   range: string
 }, spinner: Spinner): Promise<void> {
   const { filePath, sheetName, range: inputRange } = args
-  spinner.start()
   const parsedFile = parse(filePath)
   const workbook = XLSX.readFile(filePath, { raw: true, cellDates: true, dense: true, sheet: sheetName })
-  const parsingConfig: FileParserOptions = { workbook, sheetName, inputRange, parsedFile, filePath, spinner }
+  let csvSize = fs.statSync(filePath).size
+  let splitWorksheet = false
+  let outputFilePath = `${parsedFile.dir}/${parsedFile.name}_${sheetName}_${dayjs().format('YYYY.MM.DD HH.mm.ss')}`
+  if (csvSize > 5000000) {
+    splitWorksheet = await confirm({ message: 'The file is large. Would you like to split the output into multiple CSVs?', default: false })
+    if (splitWorksheet) {
+      ensureDirSync(join(parsedFile.dir, dayjs().format('YYYY.MM.DD HH.mm.ss')))
+      emptyDirSync(join(parsedFile.dir, dayjs().format('YYYY.MM.DD HH.mm.ss')))
+      outputFilePath = join(parsedFile.dir, dayjs().format('YYYY.MM.DD HH.mm.ss'), `${parsedFile.name} ${sheetName}`)
+      const tempCSVSize = await number({ message: 'Size of output CSV files (in Mb):', default: ceil(csvSize / 1000000, 2), min: 1, max: ceil(csvSize / 1000000, 2) * 4 })
+      csvSize = tempCSVSize * 1000000
+    }
+    else {
+      csvSize = csvSize * 10
+    }
+  }
+  spinner.start()
+  const parsingConfig: FileParserOptions = { workbook, sheetName, inputRange, parsedFile, spinner, outputFilePath, csvSize, splitWorksheet }
   processWorksheet(parsingConfig)
 }
 interface FileParserOptions {
@@ -35,52 +52,85 @@ interface FileParserOptions {
   sheetName: string
   inputRange: string
   parsedFile: ParsedPath
-  filePath: string
   spinner: Spinner
+  outputFilePath: string
+  csvSize: number
+  splitWorksheet: boolean
 }
 
-function processWorksheet({ workbook, sheetName, inputRange, parsedFile, filePath, spinner }: FileParserOptions): void {
+async function processWorksheet({ workbook, sheetName, inputRange, parsedFile, spinner, outputFilePath, csvSize, splitWorksheet }: FileParserOptions): void {
   const rawSheet = workbook!.Sheets[sheetName]
   const decodedRange = XLSX.utils.decode_range(inputRange)
-  const dataForCSV: Papa.UnparseObject<JsonValue> = { fields: [], data: [] }
   /* let fields: string[] = []
-     const data: JsonValue[] = [] */
+  const data: JsonValue[] = [] */
   const columnIndices = range(decodedRange.s.c, decodedRange.e.c + 1)
   const rowIndices = range(decodedRange.s.r, decodedRange.e.r + 1)
-  rowIndices.forEach((rowIdx) => {
-    if (rowIdx === decodedRange.s.r) {
-      columnIndices.forEach((colIdx) => {
-        const currentCell = rawSheet['!data'][rowIdx][colIdx]
-        if (isUndefined(currentCell)) {
-          dataForCSV.fields.push(null)
-        }
-        else {
-          dataForCSV.fields.push(currentCell.v as string)
-        }
+  let fileNum = 1
+  let writeStream = fs.createWriteStream(`${outputFilePath}_${padStart(`${fileNum}`, 3, '0')}.csv`, 'utf-8')
+  writeStream.on('close', () => {
+    fileNum += 1
+  })
+  const pass = new PassThrough()
+  pass.on('data', (chunk) => {
+    pass.pause()
+    const streamWriteResult = writeStream.write(chunk)
+    if (!splitWorksheet) {
+      pass.resume()
+    }
+    else if (!streamWriteResult) {
+      writeStream.once('drain', () => {
+        ({ writeStream, fileNum } = updateWriteStream(writeStream, csvSize, pass, fileNum, outputFilePath))
       })
-      dataForCSV.fields.push('source_file', 'source_sheet', 'source_range')
     }
     else {
-      const rowdata: JsonValue[] = []
-      columnIndices.forEach((colIdx) => {
-        const currentCell = rawSheet['!data'][rowIdx][colIdx]
-        if (isUndefined(currentCell)) {
-          rowdata.push(null)
-        }
-        else {
-          rowdata.push(currentCell.v as string)
-        }
-      })
-      rowdata.push(parsedFile.base, sheetName, inputRange)
-      dataForCSV.data.push(rowdata)
+      ({ writeStream, fileNum } = updateWriteStream(writeStream, csvSize, pass, fileNum, outputFilePath))
     }
   })
-  const csv = Papa.unparse(dataForCSV)
-  fs.writeFile(`${parsedFile.dir}/${parsedFile.name}_${sheetName}_${dayjs().format('YYYY.MM.DD HH.mm.ss')}.csv`, csv, {
-    encoding: 'utf-8',
-  }, (err) => {
-    if (err)
-      throw new Commander.InvalidArgumentError(`There was an error parsing the worksheet ${colors.bold(colors.cyan(`"${sheetName}"`))} from the Excel at ${colors.yellow(`"${filePath}"`)}`)
-    spinner.success(colors.green('Parsed successfully'))
+  pass.on('end', () => {
+    spinner.success(
+      colors.green('Parsed successfully'),
+    )
   })
+  rowIndices.forEach((rowIdx) => {
+    const rowdata: JsonValue[] = []
+    columnIndices.forEach((colIdx) => {
+      const currentCell = rawSheet['!data']?.[rowIdx]?.[colIdx]
+      rowdata.push((currentCell?.v ?? null) as string)
+    })
+    if (rowIdx === decodedRange.s.r) {
+      rowdata.push('source_file', 'source_sheet', 'source_range')
+      const csv = Papa.unparse([rowdata])
+      if (splitWorksheet)
+        fs.writeFileSync(`${outputFilePath}_HEADER.csv`, csv, 'utf-8')
+      else pass.write(`${csv}\n`)
+    }
+    else {
+      rowdata.push(parsedFile.base, sheetName, inputRange)
+      const csv = Papa.unparse([rowdata])
+      pass.write(`${csv}\n`)
+    }
+    if (rowIdx === decodedRange.e.r) {
+      pass.end()
+    }
+    // dataForCSV.push(rowdata)
+  })
+  /* fs.writeFile(`${parsedFile.dir}/${parsedFile.name}_${sheetName}_${dayjs().format('YYYY.MM.DD HH.mm.ss')}.csv`, csv, {
+       encoding: 'utf-8',
+     }, (err) => {
+       if (err)
+         throw new Commander.InvalidArgumentError(`There was an error parsing the worksheet ${colors.bold(colors.cyan(`"${sheetName}"`))} from the Excel at ${colors.yellow(`"${filePath}"`)}`)
+       spinner.success(colors.green('Parsed successfully'))
+     }) */
+}
+function updateWriteStream(writeStream: fs.WriteStream, csvSize: number, pass: PassThrough, fileNum: number, outputFilePath: string): { writeStream: fs.WriteStream, fileNum: number } {
+  if (writeStream.bytesWritten < csvSize) {
+    pass.resume()
+  }
+  else {
+    writeStream.destroy()
+    fileNum += 1
+    writeStream = fs.createWriteStream(`${outputFilePath}_${padStart(`${fileNum}`, 3, '0')}.csv`, 'utf-8')
+    pass.resume()
+  }
+  return { writeStream, fileNum }
 }
