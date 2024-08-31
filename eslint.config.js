bc8f5d5f9dafba8a984b3f9eaba8722f6113ae9a
Ben Koplin <benjaminkoplin@outlook.com>
2024-08-31 05:29:29 -0500

    chore: Update eslint.config.js, deps
    Improve ESLint configuration by adding rules for duplicate imports and unused imports. Also update the multiline comment style and fix the use of variables before they are defined. Include the tsconfig.json file for TypeScript configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -7,8 +7,15 @@ export default antfu(
     rules: {
       'no-undef': 'error',
       'no-unused-vars': 'warn',
-      'ts/no-use-before-define': 'error',
+      'no-duplicate-imports': 'warn',
+      'unused-imports/no-unused-imports': 'error',
       'style/newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+      // 'style/object-property-newline': 'error',
+      'style/multiline-comment-style': ['error', 'bare-block'],
+      'ts/no-use-before-define': 'error',
+    },
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
     },
   },
 )
