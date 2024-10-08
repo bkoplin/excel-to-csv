57cfdda0c1bfcbdb7837327b477b70d536c0eae4
Ben Koplin <benjaminkoplin@outlook.com>
2024-10-08 07:43:53 -0500

    feat: split by material class

--- src/from-csv/index.ts
+++ src/from-csv/index.ts
@@ -1,22 +1,27 @@
-import { createReadStream, createWriteStream, writeFileSync } from 'node:fs'
+import { createReadStream, createWriteStream, existsSync, writeFileSync } from 'node:fs'
 import { appendFile } from 'node:fs/promises'
 import { join, parse as pathParse } from 'node:path'
 import { parse, stringify } from 'csv'
 import { parse as parseSync } from 'csv/sync'
 import { counting, omit } from 'radash'
 import { emptyDirSync, ensureDirSync } from 'fs-extra'
-import { camelCase } from 'lodash-es'
+import { camelCase, snakeCase } from 'lodash-es'
 import Papa from 'papaparse'
 import ora from 'ora'
 
 let header: string[] = []
+const indexObject: Record<string, number> = {}
+const materialClassObject: Record<string, {
+  bytes: number
+  fileNum: number
+}> = {}
 const inputFilePath = '/Users/benkoplin/Desktop/Sample Data_Installed Product_20241001.csv'
 const parsedInputFile = pathParse(inputFilePath)
 const parsedOutputFile = omit(parsedInputFile, ['base'])
 parsedOutputFile.dir = join(parsedOutputFile.dir, `${camelCase(parsedInputFile.name)} PARSE JOB`)
 parsedOutputFile.name += ' USA ONLY'
-ensureDirSync(parsedOutputFile.dir)
 emptyDirSync(parsedOutputFile.dir)
+ensureDirSync(parsedOutputFile.dir)
 const reader = createReadStream(inputFilePath, 'utf-8')
 const headerFile = createWriteStream(join(parsedOutputFile.dir, `${parsedOutputFile.name} HEADER.csv`), 'utf-8')
 const parser = parse({
@@ -32,8 +37,8 @@ const stringifier = stringify({
   quoted_empty: true,
 })
 parser.on('data', (record) => {
-//   let record
-//   if ((record = parser.read()) !== null) {
+  //   let record
+  //   if ((record = parser.read()) !== null) {
   if (header.length === 0) {
     // header = record as string[]
     const groupedColumnNames = counting(record as string[], v => v)
@@ -46,22 +51,22 @@ parser.on('data', (record) => {
       return v
     })
       .reverse()
+    header.forEach((v, i) => indexObject[v] = i)
     headerFile.end(Papa.unparse([header]))
   }
-  else if (record[55] === 'USA' && record[26] === 'System') {
-    stringifier.write(record)
+  else {
+    const UltimateConsigneeCountryName = record[indexObject.UltimateConsigneeCountryName]
+    if (UltimateConsigneeCountryName === 'USA' && record[indexObject.MaterialClassTypeName] === 'System') {
+      stringifier.write(record)
+    }
   }
 //   }
 })
 void run()
 async function run() {
   let rowCount = 0
+  const fileCount = 1
 
-  let fileCount = 1
-  let filePath = join(parsedOutputFile.dir, `${parsedOutputFile.name} ${fileCount}.csv`)
-
-  writeFileSync(filePath, '', 'utf-8')
-  let byteLength = 0
   const files = []
 
   //   ensureFileSync(filePath)
@@ -71,20 +76,33 @@ async function run() {
     // Report current line
     // Fake asynchronous operation
     const theseRows = parseSync(row, { skip_empty_lines: true })
-    rowCount += theseRows.length
-    byteLength += row.length
-    if (byteLength > (10 * 1024 * 1024)) {
-      files.push({
-        filePath,
-        byteLength,
-      })
-      fileCount += 1
-      filePath = join(parsedOutputFile.dir, `${parsedOutputFile.name} ${fileCount}.csv`)
-      writeFileSync(filePath, '', 'utf-8')
-      byteLength = row.length
-    //   byteLength = row.length
-    }
+
     for await (const thisRow of theseRows) {
+      const thisMaterialClass = thisRow[indexObject.Level2ProductCategoryDescription]
+      if (materialClassObject[thisMaterialClass] === undefined) {
+        materialClassObject[thisMaterialClass] = {
+          bytes: 0,
+          fileNum: 1,
+        }
+      }
+      let filePath = join(parsedOutputFile.dir, `${parsedOutputFile.name} ${snakeCase(thisMaterialClass)} ${materialClassObject[thisMaterialClass].fileNum}.csv`)
+      if (!existsSync(filePath))
+        writeFileSync(filePath, '', 'utf-8')
+      rowCount += theseRows.length
+      materialClassObject[thisMaterialClass].bytes += row.length
+      if (materialClassObject[thisMaterialClass].bytes > (10 * 1024 * 1024)) {
+        files.push({
+          filePath,
+          bytes: materialClassObject[thisMaterialClass].bytes,
+        })
+        materialClassObject[thisMaterialClass].fileNum += 1
+        filePath = join(parsedOutputFile.dir, `${parsedOutputFile.name} ${snakeCase(thisMaterialClass)} ${materialClassObject[thisMaterialClass].fileNum}.csv`)
+        // if (!existsSync(filePath))
+        //   writeFileSync(filePath, '', 'utf-8')
+        writeFileSync(filePath, '', 'utf-8')
+        materialClassObject[thisMaterialClass].bytes = row.length
+        //   byteLength = row.length
+      }
       const csvOutput = Papa.unparse([thisRow])
       await appendFile(filePath, `${csvOutput}\n`, 'utf-8')
     }
