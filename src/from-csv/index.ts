84f47dd8d61e4b4aa0394a3e4910d2ca3354e7ca
Ben Koplin <benjaminkoplin@outlook.com>
2024-10-08 06:17:59 -0500

    wip: more streams

--- src/from-csv/index.ts
+++ src/from-csv/index.ts
@@ -10,9 +10,11 @@ import Papa from 'papaparse'
 import fg from 'fast-glob'
 
 const records = []
+const files = []
 let header: string[] = []
 const rowCount = 0
 let fileCount = 1
+let bytesWritten = 0
 const inputFilePath = '/Users/benkoplin/Desktop/Sample Data_Installed Product_20241001.csv'
 const parsedInputFile = pathParse(inputFilePath)
 const parsedOutputFile = omit(parsedInputFile, ['base'])
@@ -21,10 +23,10 @@ parsedOutputFile.name += ' USA ONLY'
 ensureDirSync(parsedOutputFile.dir)
 const reader = createReadStream(inputFilePath, 'utf-8')
 let writer = createWriteStream(join(parsedOutputFile.dir, `${parsedOutputFile.name} ${fileCount}.csv`), 'utf-8')
-writer.on('close', () => {
-  const files = fg.sync(join(parsedOutputFile.dir, '*.csv'))
-  console.log(files)
-})
+// writer.on('close', () => {
+//   const files = fg.sync(join(parsedOutputFile.dir, '*.csv'))
+//   console.log(files)
+// })
 const headerFile = createWriteStream(join(parsedOutputFile.dir, 'header.csv'), 'utf-8')
 const parser = parse({
   bom: true,
@@ -35,13 +37,13 @@ const parser = parse({
 const stringifier = stringify({
   quoted_empty: true,
 })
-reader.on('data', (chunk) => {
-  writeData(chunk, parser)
+stringifier.on('close', () => {
+//   writer.on('close', () => {
+  const files = fg.sync(join(parsedOutputFile.dir, '*.csv'))
+  console.log(files)
+//   })
 })
 function writeData(data: Buffer | string, writeStream: InstanceType<typeof WriteStream> | InstanceType<typeof Duplex>): void {
-  reader.pause()
-  parser.pause()
-  stringifier.pause()
   if (!writeStream.write(data)) {
     writeStream.once('drain', () => {
       reader.resume()
@@ -54,52 +56,88 @@ function writeData(data: Buffer | string, writeStream: InstanceType<typeof Write
     stringifier.resume()
     parser.resume()
   }
+  if (data instanceof Buffer)
+    bytesWritten += data.length
+  else bytesWritten += Buffer.from(data).length
 }
-stringifier.on('readable', () => {
-  let row
+// stringifier.on('readable', () => {
+//   let row
 
-  while ((row = stringifier.read()) !== null) {
-    if (writer.bytesWritten > 5 * 1024 * 1024) {
-      writer.end()
-      fileCount++
-      writer = createWriteStream(join(parsedOutputFile.dir, `${parsedOutputFile.name} ${fileCount}.csv`), 'utf-8')
-      writer.on('close', () => {
-        const files = fg.sync(join(parsedOutputFile.dir, '*.csv'))
-        console.log(files)
-      })
-      writeData(row, writer)
-    }
-    else {
-      writeData(row, writer)
-    }
+//   while ((row = stringifier.read()) !== null) {
+//     if (writer.bytesWritten > 5 * 1024 * 1024) {
+//       writer.end()
+//       fileCount++
+//       writer = createWriteStream(join(parsedOutputFile.dir, `${parsedOutputFile.name} ${fileCount}.csv`), 'utf-8')
+//       writer.on('close', () => {
+//         const files = fg.sync(join(parsedOutputFile.dir, '*.csv'))
+//         console.log(files)
+//       })
+//       writeData(row, writer)
+//     }
+//     else {
+//       writeData(row, writer)
+//     }
 
-    // records.push(row.toString())
+//     // records.push(row.toString())
+//   }
+// })
+stringifier.on('data', (row: Buffer) => {
+  reader.pause()
+  parser.pause()
+  stringifier.pause()
+  if ((bytesWritten + row.length) > 5 * 1024 * 1024) {
+    writer.end()
+    fileCount++
+    writer = createWriteStream(join(parsedOutputFile.dir, `${parsedOutputFile.name} ${fileCount}.csv`), 'utf-8')
+    // writer.on('close', () => {
+    //   const files = fg.sync(join(parsedOutputFile.dir, '*.csv'))
+    //   console.log(files)
+    // })
+  }
+  writeData(row, writer)
+})
+parser.on('data', (record) => {
+  if (header.length === 0) {
+    const groupedColumnNames = counting(record as string[], v => v)
+    header = (record as string[]).reverse().map((v) => {
+      if (groupedColumnNames[v] > 1) {
+        const count = groupedColumnNames[v]
+        groupedColumnNames[v] -= 1
+        return `${v} ${count - 1}`
+      }
+      return v
+    })
+      .reverse()
+    writeData(Papa.unparse([header]), headerFile)
+  }
+  else if (record[55] === 'USA') {
+    return record
   }
 })
-parser.on('readable', () => {
-  let record
-  while ((record = parser.read()) !== null) {
-    if (header.length === 0) {
-      const groupedColumnNames = counting(record as string[], v => v)
-      header = (record as string[]).reverse().map((v) => {
-        if (groupedColumnNames[v] > 1) {
-          const count = groupedColumnNames[v]
-          groupedColumnNames[v] -= 1
-          return `${v} ${count - 1}`
-        }
-        return v
-      })
-        .reverse()
-      writeData(Papa.unparse([header]), headerFile)
-    }
-    else if (record[55] === 'USA') {
-      writeData(record, stringifier)
-    }
-  }
-})
+// parser.on('readable', () => {
+//   let record
+//   while ((record = parser.read()) !== null) {
+//     if (header.length === 0) {
+//       const groupedColumnNames = counting(record as string[], v => v)
+//       header = (record as string[]).reverse().map((v) => {
+//         if (groupedColumnNames[v] > 1) {
+//           const count = groupedColumnNames[v]
+//           groupedColumnNames[v] -= 1
+//           return `${v} ${count - 1}`
+//         }
+//         return v
+//       })
+//         .reverse()
+//       writeData(Papa.unparse([header]), headerFile)
+//     }
+//     else if (record[55] === 'USA') {
+//       writeData(record, stringifier)
+//     }
+//   }
+// })
 // Catch any error
 parser.on('error', (err) => {
   console.error(err.message)
 })
 
-//   .pipe(writer)
+reader.pipe(parser).pipe(stringifier)
