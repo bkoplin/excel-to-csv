d07542f7632cda0a6650ba60a737f34a440190cd
Ben Koplin <benjaminkoplin@outlook.com>
2024-10-15 14:39:08 -0500

    feat!: Updated options in arguments.ts
    - `src/arguments.ts`
      - Modified the `filterTypeOption` to include a preset value of 'all'
      - Changed the description for `filterTypeOption` to indicate that it now accepts a choice
    
    This change introduces a breaking change as it alters the default behavior of the `filterTypeOption`. Now, if no specific match type is provided, it will default to 'all'.

--- src/arguments.ts
+++ src/arguments.ts
@@ -92,10 +92,10 @@ export const maxFileSizeOption = new Option(
   'the maximum size of each file in MB (if not set, the files will not be split by size)',
 ).preset<undefined | string>(undefined)
   .argParser((val): number | undefined => typeof val === 'undefined' || val === null || isNaN(toNumber(val)) ? undefined : toNumber(val))
-export const filterTypeOption = new Option('--match-type', 'the type of match to use when filtering rows')
+export const filterTypeOption = new Option('--match-type [choice]', 'the type of match to use when filtering rows')
   .choices([`all`, `any`, `none`] as const)
   // .default<`all`>(`all`)
-  // .preset<`all` | `any` | `none`>(`all`)
+  .preset<`all` | `any` | `none`>(`all`)
 
 export const writeHeaderOption = new Option(
   '-h, --header [boolean]',
