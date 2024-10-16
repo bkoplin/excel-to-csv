8a751e9519fd63e91dac9c0ff36aa9ce7ce5595b
Ben Koplin <benjaminkoplin@outlook.com>
2024-10-16 05:06:10 -0500

    feat!: Refactor writeCsv function and remove SplitOptions interface
    - `src/writeCsv/index.ts`
      - Reordered imports for better organization
      - Updated the type of options in the main function to include `parsedOutputFile` and removed `inputFilePath` from it
      - Added more detailed comments throughout the code for clarity
      - Improved category determination logic with additional checks for different types of rawCategory values
      - Enhanced file writing process by adding a progress update after every thousand lines parsed
      - Adjusted error handling in Papa.parse method to provide more informative feedback on failure
    
    - `src/writeCsv/types.ts`
      - Removed unused `SplitOptions` interface
    
    These changes improve code readability, enhance error handling, and remove unnecessary elements. The change in the options type of the main function is a breaking change.

--- src/writeCsv/index.ts
+++ src/writeCsv/index.ts
@@ -1,11 +1,40 @@
-import './table-layout.d'
+import type { ParsedPath } from 'node:path'
+import type { Readable } from 'node:stream'
+import type {
+  JsonPrimitive,
+  Simplify,
+} from 'type-fest'
+import type { GlobalOptions } from '../index'
+import type { FileMetrics } from './types'
 import { createWriteStream } from 'node:fs'
 import {
   appendFile,
   readFile,
   writeFile,
 } from 'node:fs/promises'
-import type { Readable } from 'node:stream'
+import { objectEntries } from '@antfu/utils'
+import chalk from 'chalk'
+import filenamify from 'filenamify'
+import fs from 'fs-extra'
+import {
+  findIndex,
+  findLast,
+  isEmpty,
+  isNil,
+  isNull,
+  isNumber,
+  isObjectLike,
+  isString,
+  isUndefined,
+  last,
+  maxBy,
+  padStart,
+  sumBy,
+  toInteger,
+} from 'lodash-es'
+import numbro from 'numbro'
+import ora from 'ora'
+import Papa from 'papaparse'
 import {
   format,
   join,
@@ -13,43 +42,20 @@ import {
   relative,
 } from 'pathe'
 import { filename } from 'pathe/utils'
-import Table from 'table-layout'
-import numbro from 'numbro'
-import filenamify from 'filenamify'
 import {
   alphabetical,
-  isPrimitive,
   mapValues,
   pick,
   shake,
   zipToObject,
 } from 'radash'
-import {
-  findIndex,
-  findLast,
-  isEmpty,
-  isNil,
-  isNull,
-  isObjectLike,
-  isUndefined,
-  last,
-  maxBy,
-  padStart,
-  sumBy,
-} from 'lodash-es'
-import Papa from 'papaparse'
-import type { JsonPrimitive } from 'type-fest'
-import ora from 'ora'
-import chalk from 'chalk'
+import Table from 'table-layout'
 import yaml from 'yaml'
-import { objectEntries } from '@antfu/utils'
-import fs from 'fs-extra'
-import type { FileMetrics } from './types'
-import type { GlobalOptions } from '@'
+import './table-layout.d'
 
-export default async function<Options extends GlobalOptions>(inputFile: Readable, options: Options): Promise<void> {
+export default async function<Options extends Simplify<GlobalOptions & { parsedOutputFile: Omit<ParsedPath, 'base'> }>>(inputFile: Readable, options: Options): Promise<void> {
   const {
-    inputFilePath,
+    filePath: inputFilePath,
     categoryField = '',
     fileSize: maxFileSizeInMb,
     matchType,
@@ -60,22 +66,31 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
   } = options
 
   const writeHeaderOnEachFile = options.header
+
   const spinner = ora({
     hideCursor: false,
     discardStdin: false,
   })
+
   const files: Array<FileMetrics> = []
+
   const parsedInputFile = parse(inputFilePath)
+
   spinner.start(`Parsing ${chalk.cyan(filename(inputFilePath))}`)
 
   let fields: string[] = []
+
   const headerFilePath = join(parsedOutputFile.dir, `${parsedOutputFile.name} HEADER.csv`)
+
   const headerFile = createWriteStream(headerFilePath, 'utf-8')
+
   let skippedLines = 0
+
   let parsedLines = 0
+
   Papa.parse<JsonPrimitive[]>(inputFile, {
     async step(results, parser) {
-      parser.pause()
+      // parser.pause()
       if (rowCount === parsedLines) {
         parser.abort()
       }
@@ -89,9 +104,13 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
         }
         else {
           const thisRow = zipToObject(fields, results.data)
+
           parsedLines++
+
           const filtersArray = objectEntries(filters) as Array<[string, JsonPrimitive[]]>
+
           let filterTest = isEmpty(filters)
+
           if (!filterTest) {
             if (matchType === 'none') {
               filterTest = filtersArray.every(([field, value]) => !value.includes(thisRow[field]))
@@ -103,27 +122,37 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
               filterTest = filtersArray.every(([field, value]) => value.includes(thisRow[field]))
             }
           }
-
           if (filterTest) {
             const csvOutput = Papa.unparse([results.data])
+
             const csvRowLength = Buffer.from(csvOutput).length
+
             let category: string | undefined
-            const rawCategory: string | number | boolean | null | undefined = categoryField in thisRow ? thisRow[categoryField as string] : undefined
-            if (isPrimitive(rawCategory) && !isEmpty(categoryField))
-              category = isEmpty(rawCategory) ? 'EMPTY' : rawCategory
-            else if (isNull(rawCategory) && !isEmpty(categoryField))
-              category = 'NULL'
-            else
-              category = undefined
+
+            if (!(isEmpty(categoryField) || isUndefined(categoryField) || isNull(categoryField))) {
+              const rawCategory = categoryField in thisRow ? thisRow[categoryField as string] : undefined
+
+              if (isUndefined(rawCategory))
+                category = undefined
+              else if (isString(rawCategory))
+                category = isEmpty(rawCategory) ? 'EMPTY' : rawCategory
+              else if (isNull(rawCategory))
+                category = 'NULL'
+              else if (isNumber(rawCategory))
+                category = toInteger(rawCategory).toString()
+              else category = `${rawCategory}`
+            }
 
             let activeFileObject = (isNil(category) ? last(files) : findLast(files, a => a.CATEGORY === category))
 
             if (isUndefined(activeFileObject)) {
               const defaultFileNumber = (maxFileSizeInMb ? 1 : undefined)
+
               const defaultCsvFileName = generateCsvFileName({
                 fileNumber: defaultFileNumber,
                 category,
               })
+
               activeFileObject = {
                 BYTES: csvRowLength,
                 FILENUM: (maxFileSizeInMb ? 1 : undefined),
@@ -144,6 +173,7 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
             }
             else if (!isUndefined(activeFileObject) && !isUndefined(maxFileSizeInMb) && (activeFileObject.BYTES + csvRowLength) > (maxFileSizeInMb * 1024 * 1024)) {
               spinner.text = chalk.yellow(`FINISHED WITH "${filename(activeFileObject.PATH)}"`)
+
               // await delay(noop, 1500)
               const newActiveFileObject = {
                 BYTES: csvRowLength,
@@ -159,32 +189,46 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
                 }),
                 CATEGORY: category!,
               }
+
               await appendFile(newActiveFileObject.PATH, `${csvOutput}\n`, { encoding: 'utf-8' })
               files.push(newActiveFileObject)
             }
             else {
               activeFileObject.BYTES += csvRowLength
               activeFileObject.ROWS += 1
+
               const currentFileIndex = findIndex(files, { PATH: activeFileObject.PATH })
+
               files[currentFileIndex] = activeFileObject
-              spinner.text = chalk.yellow(`WRITING "${parse(activeFileObject.PATH).base}"`)
               await appendFile(activeFileObject.PATH, `${csvOutput}\n`, { encoding: 'utf-8' })
+            }
+            if ((parsedLines % 1000) === 0) {
+              const totalRows = sumBy(files, 'ROWS')
+
+              spinner.text = chalk.magentaBright(`PARSED ${numbro(parsedLines).format({ thousandSeparated: true })} LINES; `) + chalk.greenBright(`WROTE ${numbro(totalRows).format({ thousandSeparated: true })} LINES; `) + chalk.yellow(`WRITING "${parse(activeFileObject.PATH).base};"`)
             }
           }
         }
       }
-      parser.resume()
+      // parser.resume()
     },
     complete: async () => {
       const maxFileNumLength = `${maxBy(files.filter(o => typeof o.FILENUM !== 'undefined'), 'FILENUM')?.FILENUM ?? ''}`.length
+
       const parseResults = alphabetical(files, o => o.FILENUM ? `${o.CATEGORY}${padStart(`${o.FILENUM}`, maxFileNumLength, '0')}` : o.CATEGORY ?? o.PATH).map(o => pick(o, ['CATEGORY', 'ROWS', 'BYTES', 'PATH']))
+
       const parseOutputs = files.map((o) => {
         return mapValues(shake(o, v => isUndefined(v)), v => isObjectLike(v) ? !isEmpty(v) ? yaml.stringify(v) : undefined : v)
       })
+
       const parseResultsCsv = Papa.unparse(parseOutputs)
+
       const totalRows = sumBy(files, 'ROWS')
+
       const totalBytes = sumBy(files, 'BYTES')
+
       const totalFiles = files.length
+
       const summaryString = yaml.stringify({
         'TOTAL ROWS': numbro(totalRows).format({ thousandSeparated: true }),
         'TOTAL BYTES': numbro(totalBytes).format({
@@ -198,15 +242,19 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
         'TOTAL FILES': numbro(totalFiles).format({ thousandSeparated: true }),
         // 'OUTPUT FILES': parseResults,
       })
+
       fs.outputFileSync(join(parsedOutputFile.dir, '..', `PARSE AND SPLIT OUTPUT FILES.csv`), parseResultsCsv)
       fs.outputFileSync(join(parsedOutputFile.dir, '..', `PARSE AND SPLIT SUMMARY.yaml`), summaryString)
       if (writeHeaderOnEachFile) {
         for (const file of files) {
           const header = Papa.unparse([formatHeaderValues({ data: fields })])
+
           const openFile = await readFile(file.PATH, 'utf-8')
+
           await writeFile(file.PATH, `${header}\n${openFile}`, 'utf-8')
         }
       }
+
       const table = new Table(parseResults, {
         maxWidth: 600,
         ignoreEmptyColumns: true,
@@ -231,6 +279,7 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
             name: 'BYTES',
             get: (cellValue) => {
               const formattedValue = numbro(cellValue)
+
               return formattedValue.format({
                 output: 'byte',
                 spaceSeparated: true,
@@ -246,8 +295,11 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
       })
 
       const formattedParsedLines = numbro(parsedLines).format({ thousandSeparated: true })
+
       const formattedTotalRows = numbro(totalRows).format({ thousandSeparated: true })
+
       const formattedTotalFiles = numbro(totalFiles).format({ thousandSeparated: true })
+
       const formattedTotalBytes = numbro(totalBytes).format({
         output: 'byte',
         spaceSeparated: true,
@@ -256,18 +308,20 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
         mantissa: 2,
         optionalMantissa: true,
       })
+
       let spinnerText = `SUCCESSFULLY PARSED ${chalk.green(formattedParsedLines)} LINES INTO ${chalk.green(formattedTotalRows)} LINES ACROSS ${chalk.green(formattedTotalFiles)} FILES OF TOTAL SIZE ${chalk.green(formattedTotalBytes)}\n`
 
       if (writeHeaderOnEachFile)
         spinnerText += chalk.yellow(`THE HEADER IS WRITTEN TO EACH FILE\n`)
       else spinnerText += chalk.yellow(`THE HEADER FOR ALL FILES IS ${chalk.cyan(`"${parse(headerFilePath).base}"`)}\n`)
+
       spinner.stopAndPersist({
         symbol: '🚀',
         text: `${spinnerText}\n${table.toString()}`,
       })
       process.exit()
     },
-    error(error, file) {
+    error(error, _file) {
       spinner.fail(chalk.red('FAILED TO PARSE FILES\n') + error)
     },
 
@@ -283,12 +337,14 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
     category?: string | null
   } = {}): string {
     let csvFileName = parsedOutputFile.name
-    const nonAlphaNumericPattern = /[^A-Z0-9]/gi
+
+    // const nonAlphaNumericPattern = /[^A-Z0-9]/gi
 
     if (typeof category !== 'undefined' && category !== null)
       csvFileName += ` ${category}`
-      // csvFileName += ` ${category.replace(nonAlphaNumericPattern, '_')}`
-      // csvFileName += ` ${upperFirst(camelCase(category))}`
+
+    // csvFileName += ` ${category.replace(nonAlphaNumericPattern, '_')}`
+    // csvFileName += ` ${upperFirst(camelCase(category))}`
     if (typeof fileNumber !== 'undefined')
       csvFileName += ` ${padStart(`${fileNumber}`, 4, '0')}`
 
@@ -298,7 +354,9 @@ export default async function<Options extends GlobalOptions>(inputFile: Readable
 function formatHeaderValues(results: { data: JsonPrimitive[] }): string[] {
   return results.data.map((value, index, self) => {
     const occurrencesAfter = self.slice(index + 1).filter(v => v === value).length
+
     const occurrencesBefore = self.slice(0, index).filter(v => v === value).length + 1
+
     return (occurrencesAfter + occurrencesBefore) > 1 ? `${value}_${occurrencesBefore}` : `${value}`
   })
 }
--- src/writeCsv/types.ts
+++ src/writeCsv/types.ts
@@ -8,10 +8,3 @@ export interface FileMetrics {
   CATEGORY?: string
   FILTER?: Record<string, JsonPrimitive[]> | undefined
 }
-export interface SplitOptions {
-  inputFilePath: string
-  filterValues?: string[][]
-  categoryField?: string
-  maxFileSizeInMb?: number
-  writeHeaderOnEachFile?: boolean
-}
