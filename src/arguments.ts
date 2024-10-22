0b7ea6cb8839792815cf83ca2113dce5ed39f4d2
Ben Koplin <benjaminkoplin@outlook.com>
2024-10-22 11:04:10 -0500

    feat!: Updated argument parsing and options in `src/arguments.ts`
    - **src/arguments.ts**
      - Removed unused imports and added new ones from 'radash'
      - Modified the `makeFilePathOption` function to remove parserType from return type
      - Changed preset values for several options from '' to undefined
      - Simplified argParser functions for `skipLinesOption` and `rowCountOption`
      - Refactored `filterValuesOption` to improve filter parsing logic
      - Renamed option `-c, --category-field [column name]` to `-g, --group-by-category [column name]`
      - Renamed option `-s, --file-size [number]` to `-m, --max-file-size [number]`, changed its preset value and updated its argParser function
      - Renamed option `-t, --match-type [choice]` to `-t, --filter-type [choice]`
      - Renamed option `-h, --header [true|false]` to `-w, --write-header [true|false]`, changed its preset value
    
    These changes are breaking due to modifications in function outputs and arguments.

--- src/arguments.ts
+++ src/arguments.ts
@@ -1,73 +1,65 @@
 
-import type {
-  EmptyObject,
-  Integer,
-  JsonPrimitive,
-} from 'type-fest'
+import type { JsonPrimitive } from 'type-fest'
 import { Option } from '@commander-js/extra-typings'
 import chalk from 'chalk'
 import {
+  has,
   isEmpty,
   isNaN,
   toNumber,
 } from 'lodash-es'
-import { toInt } from 'radash'
+import {
+  set,
+  toInt,
+} from 'radash'
 
-export function makeFilePathOption(parserType: 'Excel' | 'CSV'): Option<'-p, --file-path [path]', 'Excel' | 'CSV', string, undefined, false, undefined> {
+export function makeFilePathOption(parserType: 'Excel' | 'CSV'): Option<'-p, --file-path [path]', undefined, string, undefined, false, undefined> {
   return new Option('-p, --file-path [path]', `the full path to the ${chalk.yellowBright(parserType)} file`)
     .default('')
-    .preset('')
+    .preset(undefined)
 }
 
 export const sheetNameOption = new Option('-n, --sheet [sheet name]', 'the sheet containing the data to parse to CSV').default(undefined)
-.preset('')
+.preset(undefined)
 
-export const sheetRangeOption = new Option('-r, --range [range]', 'the range of cells to parse in the Excel file').preset('')
+export const sheetRangeOption = new Option('-r, --range [range]', 'the range of cells to parse in the Excel file').preset(undefined)
 .default(undefined)
 
 export const includesHeaderOption = new Option('-i, --range-includes-header [true|false]', 'flag to indicate whether the range include the header row')
-  .argParser(val => val !== 'false' && val !== 'f')
-  .preset('true')
+  .preset(true)
+  .default(true, 'the input range includes a header row')
 
 export const skipLinesOption = new Option('-l, --skip-lines [number]', 'the number of rows to skip before reading the CSV file')
   .preset(-1 as const)
-  .default(-1 as const)
-  .argParser((val: `${number}` | string) => {
-    const n = toInt(val, null)
-
-    return n as null | Integer<`${typeof val}`>
-  })
+  .default(-1 as const, 'skip -1 lines (i.e., skip no lines)')
+  .argParser((val: `${number}` | string): number | -1 => toInt(val, -1 as const))
 
 export const rowCountOption = new Option('-r, --row-count [number]', 'the number of rows to parse in the CSV file')
   .preset(-1 as const)
-  .default(-1 as const)
-  .argParser((val) => {
-    const n = toInt(val, null)
-
-    return n
-  })
+  .default(-1 as const, 'parse -1 lines (i.e., parse all lines)')
+  .argParser((val: `${number}` | string): number | -1 => toInt(val, -1 as const))
 
 export const filterValuesOption = new Option(
   '-f, --row-filters [operators...]',
   `a header/value pair to apply as a filter to each row as ${`${chalk.cyan('[COLULMN NAME]')}${chalk.whiteBright(':')}${chalk.yellow('[FILTER VALUE]')}`}. \nTo apply multiple filters, input the option multiple times, e.g., \n${`${chalk.whiteBright('-f')} ${chalk.cyan('[COLULMN NAME 1]')}${chalk.whiteBright(':')}${chalk.yellow('[FILTER VALUE 1]')} \n${chalk.whiteBright('-f')} ${chalk.cyan('[COLULMN NAME 1]')}${chalk.whiteBright(':')}${chalk.yellow('[FILTER VALUE 2]')} \n${chalk.whiteBright('-f')} ${chalk.cyan('[COLULMN NAME 2]')}${chalk.whiteBright(':')}${chalk.yellow('[FILTER VALUE 3]')}`}`,
 )
   .implies({ matchType: `all` })
-  .preset(undefined as unknown as EmptyObject)
-  .default(undefined as unknown as EmptyObject)
-  .argParser<Record<string, Array<JsonPrimitive>>>((val, filters = {}) => {
-    if (typeof val !== 'undefined' && !isEmpty(val)) {
+  .preset('' as '' | `${string}:${string}`)
+  .default('' as '' | `${string}:${string}`)
+  .argParser((val: '' | `${string}:${string}` | string, filters: Record<string, JsonPrimitive[]> = {}) => {
+    if (typeof val !== 'undefined' && val.includes(':')) {
       const [key, value] = (val || '').split(':').map(v => v.trim())
 
-      if (key.length) {
-        if (!filters[key])
-          filters[key] = []
+      if (!isEmpty(key)) {
+        if (!has(filters, key))
+          filters = set(filters, key, [])
 
-        if (value.length) {
+        if (!isEmpty(value)) {
           if (!isNaN(toNumber(value))) {
             filters[key] = [...filters[key], toNumber(value)]
           }
-          else if (value === 'true' || value === 'false') {
-            filters[key] = [...filters[key], value === 'true']
+          else if (['true', 'false', 't', 'f'].includes(value.toLowerCase())) {
+            filters[key] = [...filters[key], ['true', 't'].includes(value.toLowerCase())]
           }
           else {
             filters[key] = [...filters[key], value]
@@ -83,29 +75,31 @@ export const filterValuesOption = new Option(
   })
 
 export const categoryOption = new Option(
-  '-c, --category-field [column name]',
+  '-g, --group-by-category [column name]',
   'the name of a column whose value will be used to create each separate file',
 )
   .default<undefined | string>(undefined)
   .preset<undefined | string>(undefined)
 
 export const maxFileSizeOption = new Option(
-  '-s, --file-size [number]',
+  '-m, --max-file-size [number]',
   'the maximum size of each file in MB (if not set, the files will not be split by size)',
-).preset<undefined | string>(undefined)
-.argParser((val): number | undefined => typeof val === 'undefined' || val === null || isNaN(toNumber(val)) ? undefined : toNumber(val))
+)
+  .preset(undefined)
+  .default(0 as const, 'max file size is (0), so unlimited')
+  .argParser<number | 0>((val: `${number}` | string) => toInt(val, 0 as const))
 
-export const filterTypeOption = new Option('-t, --match-type [choice]', 'the type of match to use when filtering rows')
+export const filterTypeOption = new Option('-t, --filter-type [choice]', 'the type of match to use when filtering rows')
   .choices([`all`, `any`, `none`] as const)
-  // .default<`all`>(`all`)
+  .default<`all`>(`all`, 'each row against which the filter is compared must satisfy all filter tests')
   .preset<`all` | `any` | `none`>(`all`)
 
 export const writeHeaderOption = new Option(
-  '-h, --header [true|false]',
+  '-w, --write-header [true|false]',
   'enable/disable writing the CSV header to each file (if you select this option, the header will be written separately even if there is only one file)',
 )
-  .argParser(val => val !== 'false' && val !== 'f')
-  .preset('true')
+  .preset(true)
+  .default(false, 'do not write the header to each file')
 
 export function parseRowFilters(filters: string[]): Record<string, JsonPrimitive[]> {
   return filters.reduce((acc: Record<string, Array<JsonPrimitive>> = {}, filter): Record<string, Array<JsonPrimitive>> => {
