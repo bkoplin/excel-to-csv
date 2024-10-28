f0344fb85267acaaf2c3f3064621d6dd0ca358a1
Ben Koplin <benjaminkoplin@outlook.com>
2024-10-28 03:27:59 -0500

    feat: Update default value in categoryField.ts
    - `src/options/categoryField.ts`
      - Changed the default value for the `--category-field` option from an empty string to `undefined`. This change reflects a more accurate representation of the absence of a specified column name for splitting data into separate files.

--- src/options/categoryField.ts
+++ src/options/categoryField.ts
@@ -5,7 +5,7 @@ export default new Option(
   '--category-field [column name]',
   'the name of the column(s) whose value(s) will be used to split the data into separate files',
 )
-  .default<[]>('', 'records will not be split into separate files')
+  .default<[]>(undefined, 'records will not be split into separate files')
   .preset<[]>(undefined)
   .argParser<string[]>((val, previous = []): string[] => {
     if (!isEmpty(val)) {
