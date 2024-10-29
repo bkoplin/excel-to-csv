87a4c050c0ab69ea24b7d210b602aa5ab1875201
Ben Koplin <benjaminkoplin@outlook.com>
2024-10-29 05:57:46 -0500

    feat: Update delimiter option description in src/options/delimiter.ts
    - `src/options/delimiter.ts`
      - Updated the description of the '--delimiter [string]' option to clarify its purpose. It now indicates that it's used to parse the CSV file, not just as a generic CSV delimiter. This change improves user understanding of this command line argument's functionality.

--- src/options/delimiter.ts
+++ src/options/delimiter.ts
@@ -3,6 +3,6 @@ import { Option } from '@commander-js/extra-typings'
 
 const csvDelimiters = [`,`, `;`, `|`, `\t`] as const
 
-export default new Option<'--delimiter [string]', undefined, `,`, undefined, false, IterableElement<typeof csvDelimiters>>('--delimiter [string]', 'the CSV delimiter to use')
+export default new Option<'--delimiter [string]', undefined, `,`, undefined, false, IterableElement<typeof csvDelimiters>>('--delimiter [string]', 'the CSV delimiter to use to parse the file')
   .default(',')
   .choices(csvDelimiters)
